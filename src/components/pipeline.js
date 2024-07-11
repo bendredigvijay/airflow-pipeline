97e20238184dc44f28c3a0a493110ab69aff228d
digvijay <151820315+bendredigvijay@users.noreply.github.com>
2024-07-11 14:58:01 +0530

    SAVEChanges

--- src/components/pipeline.js
+++ src/components/pipeline.js
@@ -6,27 +6,84 @@ import { MdDeleteForever } from 'react-icons/md';
 
 const data = [
   {
-    id: 1,
-    name: { firstName: "John", lastName: "Doe" },
-    environment: "Production",
-    noOfDAGs: 5,
-    createdOn: "2023-05-15",
-    createdBy: "Alice Smith",
+    "id": 1,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
   },
   {
-    id: 2,
-    name: { firstName: "Jane", lastName: "Doe" },
-    environment: "Development",
-    noOfDAGs: 3,
-    createdOn: "2023-06-20",
-    createdBy: "Bob Johnson",
+    "id": 2,
+    "name": { "firstName": "Jane", "lastName": "Doe" },
+    "environment": "Development",
+    "noOfDAGs": 3,
+    "createdOn": "2023-06-20",
+    "createdBy": "Bob Johnson"
   },
+  {
+    "id": 3,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 4,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 5,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 6,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 7,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 8,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  },
+  {
+    "id": 9,
+    "name": { "firstName": "John", "lastName": "Doe" },
+    "environment": "Production",
+    "noOfDAGs": 5,
+    "createdOn": "2023-05-15",
+    "createdBy": "Alice Smith"
+  }
 ];
 
 const Pipeline = () => {
   const [dataState, setDataState] = useState(data);
   const [editItem, setEditItem] = useState(null); // State for edit item
   const [editModalOpen, setEditModalOpen] = useState(false);
+  const [createModalOpen, setCreateModalOpen] = useState(false); // State for create modal
 
   const columns = useMemo(
     () => [
@@ -90,7 +147,7 @@ const Pipeline = () => {
     setDataState([...dataState, clonedItem]);
   };
 
-  const handleSave = () => {
+  const handleSaveEdit = () => {
     const updatedData = dataState.map((item) =>
       item.id === editItem.id ? editItem : item
     );
@@ -98,90 +155,120 @@ const Pipeline = () => {
     setEditModalOpen(false);
   };
 
-  const handleCancel = () => {
+  const handleCancelEdit = () => {
     setEditItem(null);
     setEditModalOpen(false);
   };
 
+  const handleSaveCreate = () => {
+    // Handle save for create modal
+    setCreateModalOpen(false);
+  };
+
+  const handleCancelCreate = () => {
+    setCreateModalOpen(false);
+  };
+
   return (
-<div className="d-flex flex-column vh-100">
-  <div className="d-flex justify-content-between align-items-center mb-3" >
-    <h1 className="flex-grow-1">Pipeline Dashboard</h1>
-    <div className="mb-3" style={{ position: "fixed", right: 17, top: 40 }}>      <Button onClick={() => { /* Handle Create Button Click */ }} color="blue">
-        Create
-      </Button>
-    </div>
-  </div>
-
-  <div className="flex-1 overflow-auto cbm-wrapper d-flex justify-content-center">
-    <div style={{ margin: "1%" }}>
-      <MantineReactTable table={table} />
-    </div>
-  </div>
-
-  <Modal
-    opened={editModalOpen}
-    onClose={handleCancel}
-    title="Edit Item"
-  >
-    {editItem && (
-      <div>
-        <TextInput
-          label="First Name"
-          value={editItem.name.firstName}
-          onChange={(e) =>
-            setEditItem({
-              ...editItem,
-              name: { ...editItem.name, firstName: e.target.value },
-            })
-          }
-        />
-        <TextInput
-          label="Environment"
-          value={editItem.environment}
-          onChange={(e) =>
-            setEditItem({ ...editItem, environment: e.target.value })
-          }
-        />
-        <TextInput
-          label="No of DAGs"
-          value={editItem.noOfDAGs}
-          onChange={(e) =>
-            setEditItem({
-              ...editItem,
-              noOfDAGs: parseInt(e.target.value),
-            })
-          }
-        />
-        <TextInput
-          label="Created On"
-          value={editItem.createdOn}
-          onChange={(e) =>
-            setEditItem({ ...editItem, createdOn: e.target.value })
-          }
-        />
-        <TextInput
-          label="Created By"
-          value={editItem.createdBy}
-          onChange={(e) =>
-            setEditItem({ ...editItem, createdBy: e.target.value })
-          }
-        />
-        <div style={{ marginTop: '20px' }}>
-          <Button onClick={handleSave} style={{ marginRight: '10px' }}>
-            Save
-          </Button>
-          <Button onClick={handleCancel} color="red">
-            Cancel
+    <div className="d-flex flex-column vh-100">
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <h1 className="flex-grow-1">Pipeline Dashboard</h1>
+        <div className="mb-3" style={{ position: "fixed", right: 17, top: 40 }}>
+          <Button onClick={() => setCreateModalOpen(true)} color="blue">
+            Create
           </Button>
         </div>
       </div>
-    )}
-  </Modal>
-</div>
+
+      <div className="flex-1 overflow-auto cbm-wrapper d-flex justify-content-center">
+        <div style={{ margin: "1%" }}>
+          <MantineReactTable table={table} />
+        </div>
+      </div>
+
+      <Modal
+        opened={editModalOpen}
+        onClose={handleCancelEdit}
+        title="Edit Item"
+      >
+        {editItem && (
+          <div>
+            <TextInput
+              label="First Name"
+              value={editItem.name.firstName}
+              onChange={(e) =>
+                setEditItem({
+                  ...editItem,
+                  name: { ...editItem.name, firstName: e.target.value },
+                })
+              }
+            />
+            <TextInput
+              label="Environment"
+              value={editItem.environment}
+              onChange={(e) =>
+                setEditItem({ ...editItem, environment: e.target.value })
+              }
+            />
+            <TextInput
+              label="No of DAGs"
+              value={editItem.noOfDAGs}
+              onChange={(e) =>
+                setEditItem({
+                  ...editItem,
+                  noOfDAGs: parseInt(e.target.value),
+                })
+              }
+            />
+            <TextInput
+              label="Created On"
+              value={editItem.createdOn}
+              onChange={(e) =>
+                setEditItem({ ...editItem, createdOn: e.target.value })
+              }
+            />
+            <TextInput
+              label="Created By"
+              value={editItem.createdBy}
+              onChange={(e) =>
+                setEditItem({ ...editItem, createdBy: e.target.value })
+              }
+            />
+            <div style={{ marginTop: '20px' }}>
+              <Button onClick={handleSaveEdit} style={{ marginRight: '10px' }}>
+                Save
+              </Button>
+              <Button onClick={handleCancelEdit} color="red">
+                Cancel
+              </Button>
+            </div>
+          </div>
+        )}
+      </Modal>
+
+      <Modal
+        opened={createModalOpen}
+        onClose={handleCancelCreate}
+        title={<h3 style={{ marginRight: '10px' , fontWeight: 'bold' }}>Create Item</h3>}
+      >
+        <div>
+          <Button style={{ marginBottom: '10px', width: '100%' }}>Data Extraction</Button>
+          <Button style={{ marginBottom: '10px', width: '100%' }}>Data Transformation</Button>
+          <Button style={{ marginBottom: '10px', width: '100%' }}>Run Module</Button>
+          <div style={{ marginTop: '20px', textAlign: 'center' }}>
+          <Button onClick={handleCancelCreate} style={{ marginRight: '10px' }} color="red">
+              Cancel
+            </Button>
+            <Button onClick={handleSaveCreate} >
+              Save
+            </Button>
+           
+          </div>
+        </div>
+      </Modal>
 
 
-
+    </div>
   );
 };
 
